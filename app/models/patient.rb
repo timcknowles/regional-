ae69833536bdb308a2859750ffd3b5f1bd188d66
Tim Knowles <tim.c.knowles@gmail.com>
2016-09-03 17:30:42 +0100

    add virtual attributes for full name

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,16 @@
 class Patient < ApplicationRecord
   has_many :anaesthetics
   has_many :doctors, through: :anaesthetics
+
+  # Getter
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+
+  # Setter
+  def full_name=(name)
+    split = name.split(' ', 2)
+    self.first_name = split.first
+    self.last_name = split.last
+  end
 end
